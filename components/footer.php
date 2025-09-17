2c8c221fd560a3f0c0835438f4df3db6dd88e31b
amitkriplani14 <amitkriplani14@gmail.com>
2025-09-17 18:49:51 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +91 7262937736</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +91 8605405416 </a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> amitkriplani14@gmail.com </a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Sindhi camp Pakki kholi Akola, india - 444004 </a>
+      </div>
+
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span> Designed by Siddhant and Santosh and Guided by Dr. Dipika R Biyani Mam  </span> | all rights reserved!</div>
+
+</footer>
